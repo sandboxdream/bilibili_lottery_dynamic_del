ae415f0e6e63dc24265eb7dbd3533e6383b5ffd2
sandboxdream <1450223046@qq.com>
2021-12-31 20:49:59 +0800

    修复issue#3的问题 并增加一键删除的功能，需要在config中设置

--- main.py
+++ main.py
@@ -43,42 +43,57 @@ while res.text != over_text:
         card_js = json.loads(data1['card'])
         is_cj = 0
         dy_id = data1['desc']['dynamic_id']
-        try:
-
-            origin = card_js['origin']
-            origin_js = json.loads(origin)
-            res = origin_js['item']
+        if config['del_all'] == 0:
+            print('部分删除模式，即将判断是否抽奖过期')
             try:
-                res = res['description']
-                ans = res.find('互动抽奖')
-                if ans != -1:
-                    is_cj = 1
+
+                origin = card_js['origin']
+                origin_js = json.loads(origin)
+                res = origin_js['item']
+                try:
+                    res = res['description']
+                    ans = res.find('互动抽奖')
+                    if ans != -1:
+                        is_cj = 1
+                except KeyError:
+                    print('这个转发的视频')
             except KeyError:
-                print('这个转发的视频')
-        except KeyError:
-            print("此动态无原产地信息(非转发)")
-        if is_cj == 1:
-            orig_dy_id = card_js['item']['orig_dy_id']
-            print("检查到抽奖动态" + str(dy_id) + ' orig_id:', orig_dy_id)
-            bltime = get_bless_time(orig_dy_id)
-            if bltime == -1:
-                print("动态", dy_id, '抽奖错误，请手动排除。已经跳过')
-                continue
-            ntime = time.time()
-            ntime = int(ntime)
-            if ntime < bltime:
-                print('未开奖，不删除', dy_id, ' 当前时间', ntime, ' 开奖时间为', bltime)
-            else:
-                print('已开奖，发送删除请求', dy_id, ' 当前时间', ntime, ' 开奖时间为', bltime)
-                deled_number = deled_number + 1
-                print('已删除', deled_number, '个')
-                cookie = {'_uuid': config['_uuid'],
-                          'SESSDATA': config['SESSDATA']}
-                podata = {'dynamic_id': dy_id}
-                print(requests.post('https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/rm_dynamic',
-                                    data=podata,
-                                    cookies=cookie).text)
+                print("此动态无原产地信息(非转发)")
+            if is_cj == 1:
+                orig_dy_id = card_js['item']['orig_dy_id']
+                print("检查到抽奖动态" + str(dy_id) + ' orig_id:', orig_dy_id)
+                bltime = get_bless_time(orig_dy_id)
+                if bltime == -1:
+                    print("动态", dy_id, '抽奖错误，请手动排除。已经跳过')
+                    continue
+                ntime = time.time()
+                ntime = int(ntime)
+                if ntime < bltime:
+                    print('未开奖，不删除', dy_id, ' 当前时间', ntime, ' 开奖时间为', bltime)
+                else:
+                    print('已开奖，发送删除请求', dy_id, ' 当前时间', ntime, ' 开奖时间为', bltime)
+                    deled_number = deled_number + 1
+                    print('已删除', deled_number, '个')
+                    cookie = {'_uuid': config['_uuid'],
+                              'SESSDATA': config['SESSDATA']}
+                    podata = {'dynamic_id': dy_id}
+                    print(requests.post('https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/rm_dynamic',
+                                        data=podata,
+                                        cookies=cookie).text)
+        else:
+            print('全部删除模式，即将删除', dy_id)
+            time.sleep(1)
+            cookie = {'_uuid': config['_uuid'],
+                      'SESSDATA': config['SESSDATA']}
+            podata = {'dynamic_id': dy_id}
+
+            print(requests.post('https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/rm_dynamic',
+                                data=podata,
+                                cookies=cookie).text)
+
+
 
     nextoffid = data['data']['next_offset']
-    res = requests.get(url(nextoffid))
+    res = requests.get(url(config['uid'], nextoffid))
     print('加载下一页 下一页id为', nextoffid)
+print('查询已经结束，删除完成')
